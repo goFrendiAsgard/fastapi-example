2699df790f151406536d573078f552c224f80c06
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-09-20 17:40:27 +0700

    add arithmetic operation

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> str:
     return "Halo apa kabar"
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/add/{num1}/{num2}")
+def add(num1: int, num2: int) -> int:
+    return num1+num2
